98a2e708ab0bbbc148defd281a66e9a44ed6a03b
LeomharT <leo.liao.lzy@gmail.com>
2026-06-16 14:59:09 +0800

    feat(canvas): initialize three.js renderer

--- src/canvas.ts
+++ src/canvas.ts
@@ -1,62 +1,35 @@
 import { Colors } from '@blueprintjs/colors';
-import type { Vector2Like } from 'three';
+import { Color, PerspectiveCamera, Scene, WebGLRenderer } from 'three';
 import './style.css';
 
 const sizes = {
   width: window.innerWidth,
   height: window.innerHeight,
+  pixelRatio: Math.min(2, window.devicePixelRatio),
 };
 
 const el = document.querySelector('#root');
 
-const canvas = document.createElement('canvas');
-canvas.width = sizes.width;
-canvas.height = sizes.height;
-canvas.style.widows = sizes.width + 'px';
-canvas.style.height = sizes.height + 'px';
-el?.append(canvas);
+const renderer = new WebGLRenderer({
+  antialias: true,
+  alpha: true,
+  powerPreference: 'high-performance',
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(sizes.pixelRatio);
+el?.append(renderer.domElement);
 
-const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+const scene = new Scene();
+scene.background = new Color(Colors.BLACK);
 
-function clear() {
-  ctx.save();
-  ctx.fillStyle = Colors.BLACK;
-  ctx.fillRect(0, 0, sizes.width, sizes.height);
-  ctx.restore();
-}
+const camera = new PerspectiveCamera(35, sizes.width / sizes.height, 1, 1100);
+camera.position.set(3, 3, 3);
+camera.lookAt(scene.position);
 
-function point(v: Vector2Like) {
-  ctx.save();
-
-  const s = 30;
-  ctx.fillStyle = Colors.VIOLET4;
-  ctx.fillRect(v.x - s / 2, v.y - s / 2, s, s);
-
-  ctx.restore();
-}
-
-function renderCircle(dt: number) {
-  dt *= 0.001;
-
-  for (let i = 0; i < 10; i++) {
-    const angle = (i / 10) * Math.PI * 2;
-
-    const x = Math.cos(angle + dt) * 400 + sizes.width / 2;
-    const y = Math.sin(angle + dt) * 400 + sizes.height / 2;
-
-    point({ x, y });
-  }
-}
-
-let prev = 0;
-
-function render(time: number = 0) {
-  const dt = time - prev;
-  prev = time;
-
-  clear();
-  renderCircle(prev);
-
+function render() {
+  // Render
+  renderer.render(scene, camera);
+  // Animation
   requestAnimationFrame(render);
 }
 render();
@@ -65,8 +38,8 @@ window.addEventListener('resize', () => {
   sizes.width = window.innerWidth;
   sizes.height = window.innerHeight;
 
-  canvas.width = sizes.width;
-  canvas.height = sizes.height;
-  canvas.style.widows = sizes.width + 'px';
-  canvas.style.height = sizes.height + 'px';
+  renderer.setSize(sizes.width, sizes.height);
+
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
 });
