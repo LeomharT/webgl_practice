b2bf6326075cb9a37855a4cf26b2996c9abb1051
LeomharT <leo.liao.lzy@gmail.com>
2026-07-14 15:55:10 +0800

    feat: particles morph

--- src/canvas.ts
+++ src/canvas.ts
@@ -1,24 +1,36 @@
 import { Colors } from '@blueprintjs/colors';
+import gsap from 'gsap';
 import {
+  AdditiveBlending,
+  BufferAttribute,
+  BufferGeometry,
   Color,
-  DoubleSide,
+  Float32BufferAttribute,
   Mesh,
-  MeshBasicMaterial,
-  NearestFilter,
-  NearestMipMapNearestFilter,
   PerspectiveCamera,
-  PlaneGeometry,
+  Points,
   Scene,
+  ShaderChunk,
   ShaderMaterial,
-  SRGBColorSpace,
-  TextureLoader,
+  Uniform,
+  Vector2,
   WebGLRenderer,
-  WebGLRenderTarget,
 } from 'three';
-import { OrbitControls } from 'three/examples/jsm/Addons.js';
+import { DRACOLoader, GLTFLoader, OrbitControls } from 'three/examples/jsm/Addons.js';
 import { Pane } from 'tweakpane';
-import { PackedMipMapGenerator } from './lib/custom-mipmap-generation/PackedMipMapGenerator';
+import simplex3DNoise from './shader/include/simplex3DNoise.glsl?raw';
+import fragmentShader from './shader/test/fragment.glsl?raw';
+import vertexShader from './shader/test/vertex.glsl?raw';
 import './style.css';
+
+(ShaderChunk as any)['simplex3DNoise'] = simplex3DNoise;
+
+const dracoloader = new DRACOLoader();
+dracoloader.setDecoderPath('/draco/');
+dracoloader.preload();
+
+const gltfLoader = new GLTFLoader();
+gltfLoader.setDRACOLoader(dracoloader);
 
 const sizes = {
   width: window.innerWidth,
@@ -27,15 +39,6 @@ const sizes = {
 };
 
 const el = document.querySelector('#root');
-
-const textureLoader = new TextureLoader();
-
-const params = {
-  linearFilter: false,
-  mipLevel: 0,
-  sampleType: 0,
-  powerOfTwo: false,
-};
 
 // Basic
 const renderer = new WebGLRenderer({
@@ -47,63 +50,113 @@ renderer.setPixelRatio(sizes.pixelRatio);
 el?.append(renderer.domElement);
 
 const scene = new Scene();
-scene.background = new Color(Colors.BLACK);
+scene.background = new Color(Colors.VIOLET1).multiplyScalar(0.3);
 
 const camera = new PerspectiveCamera(40, sizes.width / sizes.height, 0.01, 1000);
-camera.position.set(0, 0, 3);
+camera.position.set(0, 0, 20);
 camera.lookAt(scene.position);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 controls.dampingFactor = 0.04;
 
-const linearTarget = new WebGLRenderTarget();
-const nearestTarget = new WebGLRenderTarget();
-nearestTarget.texture.minFilter = NearestFilter;
-nearestTarget.texture.magFilter = NearestFilter;
-
-const imgTexture = textureLoader.load('voyager-record-cover.jpg', () => {
-  updateTexture();
-});
-imgTexture.colorSpace = SRGBColorSpace;
-
 // World
-const planeGeometry = new PlaneGeometry(1, 1, 32, 32);
-const planeMaterial = new MeshBasicMaterial({
-  map: imgTexture,
-  side: DoubleSide,
+const uniforms = {
+  uSize: new Uniform(0.2),
+  uResolution: new Uniform(new Vector2(sizes.width * sizes.pixelRatio, sizes.height * sizes.pixelRatio)),
+  uProgress: new Uniform(0),
+};
+
+const particles = {
+  maxCount: 0,
+  positions: [] as BufferAttribute[],
+  index: 0,
+  morph: null as null | Function,
+};
+
+gltfLoader.load('/models.glb', (data) => {
+  const positions: BufferAttribute[] = data.scene.children.map((child) => {
+    if (child instanceof Mesh) {
+      return child.geometry.attributes.position;
+    }
+  });
+
+  // Get max count
+  for (const p of positions) {
+    if (p.count > particles.maxCount) particles.maxCount = p.count;
+  }
+
+  for (const p of positions) {
+    const originArray = p.array;
+    const newArray = new Float32Array(particles.maxCount * 3);
+
+    for (let i = 0; i < particles.maxCount; i++) {
+      const i3 = i * 3;
+
+      if (i3 < originArray.length) {
+        newArray[i3 + 0] = originArray[i3 + 0];
+        newArray[i3 + 1] = originArray[i3 + 1];
+        newArray[i3 + 2] = originArray[i3 + 2];
+      } else {
+        const randomIndex = Math.floor(Math.random() * p.count) * 3;
+
+        newArray[i3 + 0] = originArray[randomIndex + 0];
+        newArray[i3 + 1] = originArray[randomIndex + 1];
+        newArray[i3 + 2] = originArray[randomIndex + 2];
+      }
+    }
+
+    particles.positions.push(new Float32BufferAttribute(newArray, 3));
+  }
+
+  particles.morph = (index: number) => {
+    pointGeometey.setAttribute('position', particles.positions[particles.index]);
+    pointGeometey.setAttribute('aPositionTarget', particles.positions[index]);
+
+    gsap.fromTo(
+      uniforms.uProgress,
+      { value: 0 },
+      { value: 1, duration: 3, ease: 'linear', onUpdate: () => p.refresh() },
+    );
+
+    particles.index = index;
+  };
+
+  const pointGeometey = new BufferGeometry();
+  pointGeometey.setAttribute('position', particles.positions[particles.index]);
+
+  const pointMaterial = new ShaderMaterial({
+    uniforms,
+    vertexShader,
+    fragmentShader,
+    blending: AdditiveBlending,
+    depthWrite: false,
+  });
+
+  const point = new Points(pointGeometey, pointMaterial);
+  scene.add(point);
 });
-const plane = new Mesh(planeGeometry, planeMaterial);
-// plane.scale.x = 1.5;
-scene.add(plane);
 
 // Pane
 const pane = new Pane({ title: 'Debug' });
+const f_point = pane.addFolder({ title: 'Points' });
+f_point.addBinding(uniforms.uSize, 'value', {
+  step: 0.01,
+  min: 0.1,
+  max: 1,
+});
+const p = f_point.addBinding(uniforms.uProgress, 'value', {
+  label: 'progress',
+  view: 'interval',
+  step: 0.01,
+  min: 0,
+  max: 1,
+});
 
-function updateTexture() {
-  const texture = imgTexture;
-
-  texture.minFilter = NearestMipMapNearestFilter;
-  texture.generateMipmaps = false;
-
-  // render mip pyramids
-  const mipMapper = new PackedMipMapGenerator();
-  mipMapper.update(texture, nearestTarget, renderer);
-  mipMapper.update(texture, linearTarget, renderer);
-
-  // render original target
-  const copyQuad = mipMapper._copyQuad;
-  (copyQuad.material as ShaderMaterial).uniforms.tDiffuse.value = texture;
-  copyQuad.camera.setViewOffset(1, 1, 0, 0, 1, 1);
-
-  renderer.setRenderTarget(null);
-
-  // dipose
-  mipMapper.dispose();
-
-  plane.material.map = nearestTarget.texture;
-  plane.material.needsUpdate = true;
-}
+f_point.addButton({ title: 'Morphing 0' }).on('click', () => particles.morph?.(0));
+f_point.addButton({ title: 'Morphing 1' }).on('click', () => particles.morph?.(1));
+f_point.addButton({ title: 'Morphing 2' }).on('click', () => particles.morph?.(2));
+f_point.addButton({ title: 'Morphing 3' }).on('click', () => particles.morph?.(3));
 
 function render() {
   // Update
@@ -120,6 +173,7 @@ window.addEventListener('resize', () => {
   sizes.height = window.innerHeight;
 
   renderer.setSize(sizes.width, sizes.height);
+  uniforms.uResolution.value.set(sizes.width * sizes.pixelRatio, sizes.height * sizes.pixelRatio);
 
   camera.aspect = sizes.width / sizes.height;
   camera.updateProjectionMatrix();
--- src/shader/test/vertex.glsl
+++ src/shader/test/vertex.glsl
@@ -1,15 +1,41 @@
-varying vec2 vUv;
-varying vec4 vTextureUv;
+attribute vec3 aPositionTarget;
 
-uniform mat4 uTextureMatrix;
+uniform float uSize;
+uniform vec2 uResolution;
+uniform float uProgress;
+
+varying vec3 vColor;
+
+#include <simplex3DNoise>
 
 void main() {
-    vec4 modelPosition      = modelMatrix * vec4(position, 1.0);
+    float noiseOrigin = snoise(position * 0.2);
+    float noiseTarget = snoise(aPositionTarget * 0.2);
+
+    float noise = mix(noiseOrigin, noiseTarget, uProgress);
+    noise = noise * 0.5 + 0.5;
+
+    float duration = 0.1;
+    float delay    = (1.0 - duration) * noise;
+    float end      = delay + duration;
+
+    float progress = smoothstep(delay, end, uProgress);
+
+    vec3 p = mix(
+        position,
+        aPositionTarget,
+        progress
+    );
+
+    vec4 modelPosition      = modelMatrix * vec4(p, 1.0);
     vec4 viewPosition       = viewMatrix * modelPosition;
     vec4 projectionPosition = projectionMatrix * viewPosition;
 
     gl_Position = projectionPosition;
 
-    vUv        = uv;
-    vTextureUv = uTextureMatrix * vec4(position, 1.0);
+    gl_PointSize  = uSize * uResolution.y;
+    gl_PointSize *= (1.0 / -viewPosition.z);
+
+    // Varying
+    vColor = vec3(noise);
 }
