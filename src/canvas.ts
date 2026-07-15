928af04df02dbdde3ea6b6b5c44aed6d4262985a
LeomharT <leo.liao.lzy@gmail.com>
2026-07-15 09:44:15 +0800

    chore: purge

--- src/canvas.ts
+++ src/canvas.ts
@@ -1,23 +1,6 @@
 import { Colors } from '@blueprintjs/colors';
-import gsap from 'gsap';
-import {
-  AdditiveBlending,
-  BufferAttribute,
-  BufferGeometry,
-  Color,
-  Float32BufferAttribute,
-  Mesh,
-  PerspectiveCamera,
-  Points,
-  Scene,
-  ShaderChunk,
-  ShaderMaterial,
-  Uniform,
-  Vector2,
-  WebGLRenderer,
-} from 'three';
+import { Color, PerspectiveCamera, Points, Scene, ShaderChunk, SphereGeometry, WebGLRenderer } from 'three';
 import { DRACOLoader, GLTFLoader, OrbitControls } from 'three/examples/jsm/Addons.js';
-import { Pane } from 'tweakpane';
 import simplex3DNoise from './shader/include/simplex3DNoise.glsl?raw';
 import fragmentShader from './shader/test/fragment.glsl?raw';
 import vertexShader from './shader/test/vertex.glsl?raw';
@@ -50,7 +33,7 @@ renderer.setPixelRatio(sizes.pixelRatio);
 el?.append(renderer.domElement);
 
 const scene = new Scene();
-scene.background = new Color(Colors.VIOLET1).multiplyScalar(0.3);
+scene.background = new Color(Colors.BLACK);
 
 const camera = new PerspectiveCamera(40, sizes.width / sizes.height, 0.01, 1000);
 camera.position.set(0, 0, 20);
@@ -61,122 +44,20 @@ controls.enableDamping = true;
 controls.dampingFactor = 0.04;
 
 // World
-const uniforms = {
-  uSize: new Uniform(0.4),
-  uResolution: new Uniform(new Vector2(sizes.width * sizes.pixelRatio, sizes.height * sizes.pixelRatio)),
-  uProgress: new Uniform(0),
-  uColorA: new Uniform(new Color('#ff7300')),
-  uColorB: new Uniform(new Color('#0091ff')),
-};
 
-const particles = {
-  maxCount: 0,
-  positions: [] as BufferAttribute[],
-  index: 0,
-  morph: null as null | Function,
-};
+const geometry1 = new SphereGeometry(3, 32, 32);
+geometry1.setIndex(null);
 
-gltfLoader.load('/models.glb', (data) => {
-  const positions: BufferAttribute[] = data.scene.children.map((child) => {
-    if (child instanceof Mesh) {
-      return child.geometry.attributes.position;
-    }
-  });
+const uniforms = {};
 
-  // Get max count
-  for (const p of positions) {
-    if (p.count > particles.maxCount) particles.maxCount = p.count;
-  }
-
-  for (const p of positions) {
-    const originArray = p.array;
-    const newArray = new Float32Array(particles.maxCount * 3);
-
-    for (let i = 0; i < particles.maxCount; i++) {
-      const i3 = i * 3;
-
-      if (i3 < originArray.length) {
-        newArray[i3 + 0] = originArray[i3 + 0];
-        newArray[i3 + 1] = originArray[i3 + 1];
-        newArray[i3 + 2] = originArray[i3 + 2];
-      } else {
-        const randomIndex = Math.floor(Math.random() * p.count) * 3;
-
-        newArray[i3 + 0] = originArray[randomIndex + 0];
-        newArray[i3 + 1] = originArray[randomIndex + 1];
-        newArray[i3 + 2] = originArray[randomIndex + 2];
-      }
-    }
-
-    particles.positions.push(new Float32BufferAttribute(newArray, 3));
-  }
-
-  particles.morph = (index: number) => {
-    pointGeometey.setAttribute('position', particles.positions[particles.index]);
-    pointGeometey.setAttribute('aPositionTarget', particles.positions[index]);
-
-    gsap.fromTo(
-      uniforms.uProgress,
-      { value: 0 },
-      { value: 1, duration: 3, ease: 'linear', onUpdate: () => p.refresh() },
-    );
-
-    particles.index = index;
-  };
-
-  // Random Size
-  const sizeArray = new Float32Array(particles.maxCount);
-  for (let i = 0; i < particles.maxCount; i++) {
-    sizeArray[i] = Math.random();
-  }
-
-  const pointGeometey = new BufferGeometry();
-  pointGeometey.setAttribute('position', particles.positions[particles.index]);
-  pointGeometey.setAttribute('aPositionTarget', particles.positions[3]);
-  pointGeometey.setAttribute('aSize', new BufferAttribute(sizeArray, 1));
-
-  const pointMaterial = new ShaderMaterial({
-    uniforms,
-    vertexShader,
-    fragmentShader,
-    blending: AdditiveBlending,
-    depthWrite: false,
-  });
-
-  const point = new Points(pointGeometey, pointMaterial);
-  point.frustumCulled = false;
-  scene.add(point);
+const pointMaterial = new ShaderMaterial({
+  uniforms,
+  vertexShader,
+  fragmentShader,
 });
+const points = new Points(geometry1, pointMaterial);
 
-// Pane
-const pane = new Pane({ title: 'Debug' });
-const f_point = pane.addFolder({ title: 'Points' });
-f_point.addBinding(uniforms.uSize, 'value', {
-  step: 0.01,
-  min: 0.1,
-  max: 1,
-});
-const p = f_point.addBinding(uniforms.uProgress, 'value', {
-  label: 'progress',
-  view: 'interval',
-  step: 0.01,
-  min: 0,
-  max: 1,
-});
-
-f_point.addBinding(uniforms.uColorA, 'value', {
-  label: 'Color A',
-  color: { type: 'float' },
-});
-f_point.addBinding(uniforms.uColorB, 'value', {
-  label: 'Color B',
-  color: { type: 'float' },
-});
-
-f_point.addButton({ title: 'Morphing 0' }).on('click', () => particles.morph?.(0));
-f_point.addButton({ title: 'Morphing 1' }).on('click', () => particles.morph?.(1));
-f_point.addButton({ title: 'Morphing 2' }).on('click', () => particles.morph?.(2));
-f_point.addButton({ title: 'Morphing 3' }).on('click', () => particles.morph?.(3));
+scene.add(points);
 
 function render() {
   // Update
@@ -193,7 +74,6 @@ window.addEventListener('resize', () => {
   sizes.height = window.innerHeight;
 
   renderer.setSize(sizes.width, sizes.height);
-  uniforms.uResolution.value.set(sizes.width * sizes.pixelRatio, sizes.height * sizes.pixelRatio);
 
   camera.aspect = sizes.width / sizes.height;
   camera.updateProjectionMatrix();
--- src/shader/test/fragment.glsl
+++ src/shader/test/fragment.glsl
@@ -1,18 +1,1 @@
-varying vec3 vColor;
-
-
-void main() {
-    vec3 color  = vec3(1.0);
-    vec2 uv     = gl_PointCoord;
-    vec2 center = vec2(0.5);
-
-    float dist = distance(uv, center);
-    float alpha = 0.05 / dist - 0.1;
-
-    color = vColor;
-
-    gl_FragColor = vec4(color, alpha);
-
-    #include <tonemapping_fragment>
-    #include <colorspace_fragment>
-}
+ 
