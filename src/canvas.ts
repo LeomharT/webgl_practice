0b0231592d2eb0fc3e45ff1a7ae8be0a7f8b4114
LeomharT <leo.liao.lzy@gmail.com>
2026-07-24 15:30:18 +0800

    feat: purge code

--- src/canvas.ts
+++ src/canvas.ts
@@ -1,30 +1,9 @@
 import { Colors } from '@blueprintjs/colors';
 import * as EssentialsPlugin from '@tweakpane/plugin-essentials';
-import {
-  Color,
-  IcosahedronGeometry,
-  Mesh,
-  MeshBasicMaterial,
-  NearestMipMapNearestFilter,
-  PerspectiveCamera,
-  PlaneGeometry,
-  Scene,
-  ShaderChunk,
-  ShaderMaterial,
-  SRGBColorSpace,
-  TextureLoader,
-  Uniform,
-  WebGLRenderer,
-  WebGLRenderTarget,
-} from 'three';
-import { DRACOLoader, GLTFLoader, OrbitControls, Reflector } from 'three/examples/jsm/Addons.js';
+import { Color, PerspectiveCamera, Scene, ShaderChunk, SRGBColorSpace, TextureLoader, WebGLRenderer } from 'three';
+import { DRACOLoader, GLTFLoader, OrbitControls } from 'three/examples/jsm/Addons.js';
 import { Pane } from 'tweakpane';
 import simplex3DNoise from './shader/include/simplex3DNoise.glsl?raw';
-import fragmentShader from './shader/test/fragment.glsl?raw';
-import rainFragmentShader from './shader/test/rain/fragment.glsl?raw';
-import rainVertexShader from './shader/test/rain/vertex.glsl?raw';
-
-import vertexShader from './shader/test/vertex.glsl?raw';
 
 import './style.css';
 
@@ -72,61 +51,7 @@ camera.lookAt(scene.position);
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 
-const frameRT = new WebGLRenderTarget(sizes.width, sizes.height, {
-  generateMipmaps: true,
-  minFilter: NearestMipMapNearestFilter,
-});
-
 // WORLD
-const planeGeometry = new PlaneGeometry(1, 1, 32, 32);
-const floorReflector = new Reflector(planeGeometry, {
-  textureWidth: sizes.width,
-  textureHeight: sizes.height,
-});
-floorReflector.rotation.x = -Math.PI / 2;
-floorReflector.visible = false;
-(floorReflector.material as ShaderMaterial).polygonOffset = true;
-(floorReflector.material as ShaderMaterial).polygonOffsetFactor = 0.01;
-scene.add(floorReflector);
-
-const uniforms = {
-  uReflectorTexture: new Uniform(floorReflector.getRenderTarget().texture),
-  uTextureMatrix: (floorReflector.material as ShaderMaterial).uniforms.textureMatrix,
-
-  uNormalTexture: new Uniform(normalTexture),
-  uRoughnessTexture: new Uniform(roughnessTexture),
-
-  uDisturbedAmount: new Uniform(0.125),
-  uBlurStrength: new Uniform(6.125),
-};
-uniforms.uReflectorTexture.value.generateMipmaps = true;
-uniforms.uReflectorTexture.value.minFilter = NearestMipMapNearestFilter;
-
-const planeMaterial = new ShaderMaterial({
-  uniforms,
-  vertexShader,
-  fragmentShader,
-});
-const floor = new Mesh(planeGeometry, planeMaterial);
-floor.rotation.x = -Math.PI / 2;
-scene.add(floor);
-
-const ball = new Mesh(new IcosahedronGeometry(0.05, 5), new MeshBasicMaterial({ color: Colors.VIOLET3 }));
-ball.position.y = 0.1;
-scene.add(ball);
-
-const rain = new Mesh(
-  new PlaneGeometry(0.2, 0.2, 32, 32),
-  new ShaderMaterial({
-    uniforms: {
-      uFrameTexture: new Uniform(frameRT.texture),
-    },
-    vertexShader: rainVertexShader,
-    fragmentShader: rainFragmentShader,
-  }),
-);
-rain.position.set(0.2, 0.2, 0.2);
-scene.add(rain);
 
 const pane = new Pane({ title: 'Debug Pane' });
 // Register plugin to the pane
@@ -139,34 +64,12 @@ const fpsGraph: any = pane.addBlade({
 });
 
 const p_floor = pane.addFolder({ title: 'Floor' });
-p_floor.addBinding(uniforms.uDisturbedAmount, 'value', {
-  min: 0,
-  max: 1,
-  step: 0.001,
-});
-p_floor.addBinding(uniforms.uBlurStrength, 'value', {
-  min: 0,
-  max: 20,
-  step: 0.01,
-});
-function renderReflection() {
-  floorReflector.visible = true;
-  rain.visible = false;
-
-  renderer.setRenderTarget(frameRT);
-  renderer.render(scene, camera);
-  renderer.setRenderTarget(null);
-
-  rain.visible = true;
-  floorReflector.visible = false;
-}
 
 function render() {
   fpsGraph.begin();
   // Update
   controls.update();
   // Render
-  renderReflection();
   renderer.render(scene, camera);
   // Animation
   requestAnimationFrame(render);
