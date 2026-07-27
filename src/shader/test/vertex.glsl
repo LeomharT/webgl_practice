e00598691366a9b77b6d9f08be7d9f4d90b597a9
LeomharT <leo.liao.lzy@gmail.com>
2026-07-27 10:15:50 +0800

    feat: particle morph

--- src/shader/test/vertex.glsl
+++ src/shader/test/vertex.glsl
@@ -7,12 +7,21 @@ uniform vec2 uResolution;
 uniform float uSize;
 uniform float uProgress;
 
+#include <simplex3DNoise>
+
 void main() {
+    float noise = snoise(position);
+    noise = (noise + 1.0) / 2.0;
+
+    float duration = 0.4;
+    float delay    = (1.0 - duration) * noise;
+    float end      = duration + delay;
+    float progress = smoothstep(delay, end, uProgress);
 
     vec3 p = mix(
         position,
         aPositionTarget,
-        uProgress
+        progress
     );
 
     vec4 modelPosition      = modelMatrix * vec4(p, 1.0);
@@ -24,5 +33,9 @@ void main() {
     gl_PointSize  = uSize * uResolution.y * aSize;
     gl_PointSize *= 1.0 / -viewPosition.z;
 
-    vColor = vec3(1.0);
+    vColor = mix(
+        vec3(1.0, 0.0, 0.0),
+        vec3(0.0, 0.0, 1.0),
+        noise
+    );
 }
