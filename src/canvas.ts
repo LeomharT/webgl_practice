f72578769e149001fd216f192630f87eab7f3ea8
LeomharT <leo.liao.lzy@gmail.com>
2026-07-28 10:00:00 +0800

    chore: update outdate package

--- src/canvas.ts
+++ src/canvas.ts
@@ -1,20 +1,3 @@
-import * as EssentialsPlugin from '@tweakpane/plugin-essentials';
-import {
-  Color,
-  PerspectiveCamera,
-  Points,
-  Scene,
-  ShaderMaterial,
-  SphereGeometry,
-  Timer,
-  Uniform,
-  Vector2,
-  WebGLRenderer,
-} from 'three';
-import { OrbitControls } from 'three/examples/jsm/Addons.js';
-import { Pane } from 'tweakpane';
-import fragmentShader from './shader/test/fragment.glsl?raw';
-import vertexShader from './shader/test/vertex.glsl?raw';
 import './style.css';
 
 const sizes = {
@@ -22,83 +5,7 @@ const sizes = {
   height: window.innerHeight,
   pixelRatio: Math.min(2, window.devicePixelRatio),
 };
+
 const el = document.querySelector('#root');
 
-const renderer = new WebGLRenderer({
-  antialias: true,
-  alpha: true,
-});
-renderer.setSize(sizes.width, sizes.height);
-renderer.setPixelRatio(sizes.pixelRatio);
-el?.append(renderer.domElement);
-
-const scene = new Scene();
-scene.background = new Color('#000000');
-
-const camera = new PerspectiveCamera(75, sizes.width / sizes.height, 0.01, 1000);
-camera.position.set(3, 3, 3);
-camera.lookAt(scene.position);
-
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.enableDamping = true;
-
-const timer = new Timer();
-
-const uniforms = {
-  uSize: new Uniform(0.04),
-  uResolution: new Uniform(new Vector2(sizes.width, sizes.height)),
-};
-
-const sphereGeometry = new SphereGeometry(1, 32, 32);
-const pointMaterial = new ShaderMaterial({
-  uniforms,
-  fragmentShader,
-  vertexShader,
-});
-
-const points = new Points(sphereGeometry, pointMaterial);
-scene.add(points);
-
-const pane = new Pane({ title: 'Debug' });
-pane.element.parentElement!.style.width = '380px';
-pane.registerPlugin(EssentialsPlugin);
-// Add a FPS graph
-const fpsGraph: any = pane.addBlade({
-  view: 'fpsgraph',
-  label: undefined,
-  rows: 3,
-});
-
-const f_point = pane.addFolder({ title: 'Point' });
-f_point.addBinding(uniforms.uSize, 'value', {
-  label: 'Size',
-  step: 0.01,
-  min: 0,
-  max: 1,
-});
-
-function render() {
-  fpsGraph.begin();
-
-  timer.update();
-  controls.update();
-
-  //
-  renderer.render(scene, camera);
-  //
-  requestAnimationFrame(render);
-
-  fpsGraph.end();
-}
-render();
-
-window.addEventListener('resize', () => {
-  sizes.width = window.innerWidth;
-  sizes.height = window.innerHeight;
-
-  uniforms.uResolution.value.set(sizes.width, sizes.height);
-  renderer.setSize(sizes.width, sizes.height);
-
-  camera.aspect = sizes.width / sizes.height;
-  camera.updateProjectionMatrix();
-});
+console.log(root);
