b6db765383497685424af6e1b73d280e1273f67d
LeomharT <leo.liao.lzy@gmail.com>
2026-08-02 01:24:19 +0800

    feat: point shader

--- src/shader/test/fragment.glsl
+++ src/shader/test/fragment.glsl
@@ -2,6 +2,11 @@ varying vec3 vColor;
 
 void main() {
   vec3 color = vColor;
+  vec2 uv = gl_PointCoord;
+
+  float dist = length(uv - 0.5);
+
+  if (dist > 0.5) discard;
 
   gl_FragColor = vec4(color, 1.0);
 }
