0ae3d97662dad144f71502b8f90425929590360b
LeomharT <leo.liao.lzy@gmail.com>
2026-08-13 00:45:45 +0800

    feat: add pointer arc

--- src/test.ts
+++ src/test.ts
@@ -1,162 +1,61 @@
 import { Colors } from '@blueprintjs/colors';
-import gsap from 'gsap';
-import {
-  Color,
-  IcosahedronGeometry,
-  MathUtils,
-  Mesh,
-  PerspectiveCamera,
-  Scene,
-  ShaderChunk,
-  ShaderMaterial,
-  Timer,
-  Uniform,
-  Vector2,
-  WebGLRenderer,
-} from 'three';
-import {
-  DRACOLoader,
-  EffectComposer,
-  GLTFLoader,
-  OrbitControls,
-  OutputPass,
-  RenderPass,
-  UnrealBloomPass,
-} from 'three/examples/jsm/Addons.js';
-import { Pane } from 'tweakpane';
-import simplex3DNoise from './shader/include/simplex3DNoise.glsl?raw';
-import simplex4DNoise from './shader/include/simplex4DNoise.glsl?raw';
-import fragmentShader from './shader/test/fragment.glsl?raw';
-import vertexShader from './shader/test/vertex.glsl?raw';
 import './style.css';
 
-(ShaderChunk as any)['simplex3DNoise'] = simplex3DNoise;
-(ShaderChunk as any)['simplex4DNoise'] = simplex4DNoise;
-
-const dracoLoader = new DRACOLoader();
-dracoLoader.setDecoderPath('/draco/');
-
-const gltfLoader = new GLTFLoader();
-gltfLoader.dracoLoader = dracoLoader;
+const el = document.querySelector('#root');
 
 const sizes = {
   width: window.innerWidth,
   height: window.innerHeight,
-  pixelRatio: Math.min(2, window.devicePixelRatio),
 };
 
-const el = document.querySelector('#root');
+const canvas = document.createElement('canvas');
+canvas.width = sizes.width;
+canvas.height = sizes.height;
+canvas.style.width = sizes.width + 'px';
+canvas.style.height = sizes.height + 'px';
+el?.append(canvas);
 
-const renderer = new WebGLRenderer({
-  alpha: true,
-  antialias: true,
-});
-renderer.setSize(sizes.width, sizes.height);
-renderer.setPixelRatio(sizes.pixelRatio);
-el?.append(renderer.domElement);
+const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
 
-const scene = new Scene();
-scene.background = new Color(Colors.BLACK);
-
-const camera = new PerspectiveCamera(75, sizes.width / sizes.height, 0.01, 1000);
-camera.position.set(3, 3, 8);
-camera.lookAt(scene.position);
-
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.enableDamping = true;
-
-const timer = new Timer();
-
-// POST PROGRESSING
-const composer = new EffectComposer(renderer);
-composer.setSize(sizes.width, sizes.height);
-composer.setPixelRatio(sizes.pixelRatio);
-
-const renderScene = new RenderPass(scene, camera);
-const bloomPass = new UnrealBloomPass(new Vector2(sizes.width, sizes.height), 0.5, 0.5, 0.5);
-const outputPass = new OutputPass();
-
-composer.addPass(renderScene);
-composer.addPass(bloomPass);
-composer.addPass(outputPass);
-
-// WORLD
-
-const uniforms = {
-  uTime: new Uniform(0),
-  uProgress: new Uniform(0.0),
-};
-
-const params = {
+const pointer = {
+  x: 0,
   y: 0,
-  updating: false,
 };
 
-const sphereGeometry = new IcosahedronGeometry(2, 50);
-const material = new ShaderMaterial({
-  uniforms,
-  vertexShader,
-  fragmentShader,
-});
-const sphere = new Mesh(sphereGeometry, material);
-scene.add(sphere);
-
-function onProgress(value: number) {
-  gsap
-    .fromTo(
-      uniforms.uProgress,
-      { value: 1.0 - value },
-      { value: value * 1.0, ease: 'circ', duration: 2, onUpdate: () => p_progress.refresh() },
-    )
-    .play();
+function clean() {
+  ctx.save();
+  ctx.fillStyle = Colors.BLACK;
+  ctx.fillRect(0, 0, sizes.width, sizes.height);
+  ctx.restore();
 }
 
-function updatePosition(y: number, t: number) {
-  sphere.position.y = MathUtils.lerp(sphere.position.y, y, t);
+function drawPointer(x: number, y: number) {
+  ctx.save();
+  ctx.fillStyle = Colors.VIOLET3;
+  ctx.beginPath();
+  ctx.arc(x, y, 30, 0, Math.PI * 2);
+  ctx.fill();
+  ctx.restore();
 }
 
-const pane = new Pane({ title: 'Debug Pane' });
-const p_progress = pane.addBinding(uniforms.uProgress, 'value', {
-  min: 0,
-  max: 1,
-  step: 0.01,
-});
-
-pane.addButton({ title: 'Progress 0' }).on('click', () => {
-  onProgress(0);
-  params.y = 0;
-});
-pane.addButton({ title: 'Progress 1' }).on('click', () => {
-  onProgress(1);
-  params.y = 10;
-});
-
-// EVENTS
 function render() {
-  // UPDATE
-  timer.update();
-  controls.update();
-
-  const dt = timer.getDelta();
-  const t = dt;
-
-  uniforms.uTime.value += dt;
-
-  updatePosition(params.y, t);
-  // RENDER
-  composer.render();
-  // ANIMATION
+  clean();
+  drawPointer(pointer.x, pointer.y);
   requestAnimationFrame(render);
 }
 render();
+
+window.addEventListener('pointermove', (e) => {
+  pointer.x = e.clientX;
+  pointer.y = e.clientY;
+});
 
 window.addEventListener('resize', () => {
   sizes.width = window.innerWidth;
   sizes.height = window.innerHeight;
 
-  renderer.setSize(sizes.width, sizes.height);
-  composer.setSize(sizes.width, sizes.height);
-
-  camera.aspect = sizes.width / sizes.height;
-  camera.updateProjectionMatrix();
+  canvas.width = sizes.width;
+  canvas.height = sizes.height;
+  canvas.style.width = sizes.width + 'px';
+  canvas.style.height = sizes.height + 'px';
 });
