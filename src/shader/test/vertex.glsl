4049bf7e7e40e86f85b86dfac2b81eef0f12dac3
LeomharT <leo.liao.lzy@gmail.com>
2026-08-14 04:04:12 +0800

    feat: basic scene

--- src/shader/test/vertex.glsl
+++ src/shader/test/vertex.glsl
@@ -1,29 +1,4 @@
-varying vec3 vColor;
-varying vec3 vPosition;
-
-uniform float uTime;
-
-#include <simplex4DNoise>
-
-float getElevation(vec3 p) {
-  float noise = snoise(vec4(p, uTime));
-
-  return noise;
-}
-
 void main() {
-  float noise = getElevation(position);
-
-  vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-  vec3 modelNormal = transpose(inverse(mat3(modelMatrix))) * normal;
-  // modelPosition.xyz += noise * modelNormal;
-
-  vec4 viewPosition = viewMatrix * modelPosition;
-  vec4 projectionPosition = projectionMatrix * viewPosition;
-
-  gl_Position = projectionPosition;
-
-  // VARYING
-  vColor = mix(vec3(1.0, 0.0, 0.0), vec3(0.0, 1.0, 0.0), noise);
-  vPosition = modelPosition.xyz;
+  #include <begin_vertex>
+  #include <project_vertex>
 }
--- src/test.ts
+++ src/test.ts
@@ -1,5 +1,7 @@
-import { Colors } from '@blueprintjs/colors';
-import { MathUtils } from 'three';
+import { Color, Mesh, PerspectiveCamera, PlaneGeometry, Scene, ShaderMaterial, WebGLRenderer } from 'three';
+import { OrbitControls } from 'three/examples/jsm/Addons.js';
+import fragmentShader from './shader/test/fragment.glsl?raw';
+import vertexShader from './shader/test/vertex.glsl?raw';
 import './style.css';
 
 const el = document.querySelector('#root');
@@ -7,127 +9,54 @@ const el = document.querySelector('#root');
 const sizes = {
   width: window.innerWidth,
   height: window.innerHeight,
+  pixelRatio: Math.min(2, window.devicePixelRatio),
 };
 
-const canvas = document.createElement('canvas');
-canvas.width = sizes.width;
-canvas.height = sizes.height;
-canvas.style.width = sizes.width + 'px';
-canvas.style.height = sizes.height + 'px';
-el?.append(canvas);
+// BASIC
+const renderer = new WebGLRenderer({
+  alpha: true,
+  antialias: true,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(sizes.pixelRatio);
+el?.append(renderer.domElement);
 
-const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+const scene = new Scene();
+scene.background = new Color('#000');
 
-const colors = [
-  '#1F4B99',
-  '#2B609D',
-  '#3975A2',
-  '#4889A9',
-  '#5B9DB1',
-  '#72B1BB',
-  '#8BC3C7',
-  '#A8D6D5',
-  '#C9E7E4',
-  '#ECF7F6',
-  '#FFF3EC',
-  '#FBDCC7',
-  '#F5C6A5',
-  '#EDB086',
-  '#E29A69',
-  '#D78450',
-  '#CA6F3A',
-  '#BC5927',
-  '#AD4318',
-  '#9E2B0E',
-];
+const camera = new PerspectiveCamera(70, sizes.width / sizes.height, 0.01, 1000);
+camera.position.set(0.1, 0.5, 1);
+camera.lookAt(scene.position);
 
-const pointer = {
-  x: 0,
-  y: 0,
-};
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
 
-const target = {
-  x: 0,
-  y: 0,
-};
-
-const positions = Array.from({ length: 500 }, () => ({
-  x: Math.random() * sizes.width,
-  y: Math.random() * sizes.height,
-  color: colors[Math.floor(Math.random() * colors.length)],
-  length: Math.random() * 200 + 50,
-  angle: 0,
-}));
-
-function clean() {
-  ctx.save();
-  ctx.fillStyle = Colors.BLACK;
-  ctx.fillRect(0, 0, sizes.width, sizes.height);
-  ctx.restore();
-}
-
-function drawPointer(x: number, y: number) {
-  ctx.save();
-  ctx.fillStyle = Colors.VIOLET3;
-  ctx.beginPath();
-  ctx.arc(x, y, 30, 0, Math.PI * 2);
-  ctx.fill();
-  ctx.restore();
-}
-
-function drawBleads() {
-  for (const p of positions) {
-    ctx.save();
-
-    ctx.translate(p.x, p.y);
-    ctx.rotate(p.angle);
-
-    ctx.beginPath();
-    ctx.moveTo(-p.length / 2, 0);
-    ctx.lineTo(p.length / 2, 0);
-    ctx.lineWidth = 7;
-    ctx.lineCap = 'round';
-    ctx.strokeStyle = p.color;
-    ctx.stroke();
-
-    ctx.restore();
-  }
-}
-
-let prevTime = 0;
+// WORLD
+const floorGeometry = new PlaneGeometry(1, 1, 32, 32);
+const floorMaterial = new ShaderMaterial({
+  vertexShader,
+  fragmentShader,
+});
+const floor = new Mesh(floorGeometry, floorMaterial);
+floor.rotation.x = -Math.PI / 2;
+scene.add(floor);
 
 function render(time: number = 0) {
-  const dt = (time - prevTime) / 1000;
-  prevTime = time;
-
-  const t = 1.0 - Math.exp(10.0 * -dt);
-
-  target.x = MathUtils.lerp(target.x, pointer.x, t);
-  target.y = MathUtils.lerp(target.y, pointer.y, t);
-
-  for (const p of positions) {
-    const theta = Math.atan2(target.y - p.y, target.x - p.x);
-    p.angle = theta + Math.PI / 2;
-  }
-
-  clean();
-  drawPointer(target.x, target.y);
-  drawBleads();
+  // UPDATE
+  controls.update();
+  // RENDER
+  renderer.render(scene, camera);
+  // ANIMATION
   requestAnimationFrame(render);
 }
 render();
-
-window.addEventListener('pointermove', (e) => {
-  pointer.x = e.clientX;
-  pointer.y = e.clientY;
-});
 
 window.addEventListener('resize', () => {
   sizes.width = window.innerWidth;
   sizes.height = window.innerHeight;
 
-  canvas.width = sizes.width;
-  canvas.height = sizes.height;
-  canvas.style.width = sizes.width + 'px';
-  canvas.style.height = sizes.height + 'px';
+  renderer.setSize(sizes.width, sizes.height);
+
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
 });
