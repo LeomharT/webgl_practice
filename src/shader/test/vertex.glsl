dd06829b9914ef88849ffb60093722728283f22f
LeomharT <leo.liao.lzy@gmail.com>
2026-08-21 10:59:33 +0800

    feat: progress noise

--- src/shader/test/vertex.glsl
+++ src/shader/test/vertex.glsl
@@ -7,7 +7,18 @@ attribute vec3 aPositionTarget;
 #include <simplex3DNoise>
 
 void main() {
-  vec3 p = mix(position, aPositionTarget, uProgress);
+  float noiseA = snoise(position * 0.25);
+  float noiseB = snoise(aPositionTarget * 0.25);
+
+  float noise = mix(noiseA, noiseB, uProgress);
+  noise = (noise + 1.0) / 2.0;
+
+  float duration = 0.4;
+  float delay = (1.0 - duration) * noise;
+  float end = duration + delay;
+  float progress = smoothstep(delay, end, uProgress);
+
+  vec3 p = mix(position, aPositionTarget, progress);
 
   vec4 modelViewPosition = modelViewMatrix * vec4(p, 1.0);
   vec4 projectionPosition = projectionMatrix * modelViewPosition;
