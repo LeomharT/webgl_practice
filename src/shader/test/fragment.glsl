810dd00b878b80d10872c0bb7b34f92850506efe
LeomharT <leo.liao.lzy@gmail.com>
2026-08-26 11:46:02 +0800

    feat: fragment round

--- src/shader/test/fragment.glsl
+++ src/shader/test/fragment.glsl
@@ -5,7 +5,8 @@ void main() {
   vec2 uv = gl_PointCoord;
 
   float dist = length(uv - 0.5);
+  if (dist > 0.5) discard;
   float alpha = 0.05 / dist - 0.1;
 
-  gl_FragColor = vec4(color, alpha);
+  gl_FragColor = vec4(color, 1.0);
 }
