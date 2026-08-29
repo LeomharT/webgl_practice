e5769baa82bd8e1f92217787f45e0b77ba3ec815
LeomharT <leo.liao.lzy@gmail.com>
2026-08-29 19:42:36 +0800

    feat: glowing particle

--- src/shader/test/fragment.glsl
+++ src/shader/test/fragment.glsl
@@ -1,12 +1,9 @@
-varying vec3 vColor;
-
 void main() {
-  vec3 color = vColor;
-  vec2 uv = gl_PointCoord;
+  vec3 color = vec3(1.0);
+  vec2 uv    = gl_PointCoord;
 
   float dist = length(uv - 0.5);
-  if (dist > 0.5) discard;
   float alpha = 0.05 / dist - 0.1;
 
-  gl_FragColor = vec4(color, 1.0);
+  gl_FragColor = vec4(color, alpha);
 }
--- src/shader/test/vertex.glsl
+++ src/shader/test/vertex.glsl
@@ -1,21 +1,12 @@
 uniform float uSize;
 uniform vec2 uResolution;
-uniform vec2 uHeightRange;
-
-varying vec3 vColor;
 
 void main() {
-  vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-  float h = smoothstep(uHeightRange.x, uHeightRange.y, modelPosition.y);
-  h = clamp(h, 0.0, 1.0);
-
-  vec4 viewPosition = viewMatrix * modelPosition;
-  vec4 projectionPosition = projectionMatrix * viewPosition;
+  vec4 modelViewPosition  = modelViewMatrix * vec4(position, 1.0);
+  vec4 projectionPosition = projectionMatrix * modelViewPosition;
 
   gl_Position = projectionPosition;
 
-  gl_PointSize = uSize * uResolution.y;
-  gl_PointSize *= 1.0 / -viewPosition.z;
-
-  vColor = mix(vec3(1.0, 0.0, 0.0), vec3(0.0, 0.0, 1.0), h);
+  gl_PointSize  = uSize * uResolution.y;
+  gl_PointSize *= 1.0 / -modelViewPosition.z;
 }
