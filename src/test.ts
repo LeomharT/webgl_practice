6edad6d7876bd4b8308c066a78e97dca41fe3dc6
LeomharT <leo.liao.lzy@gmail.com>
2026-09-03 18:06:17 +0800

    feat: render robot marker

--- src/test.ts
+++ src/test.ts
@@ -1,26 +1,4 @@
 import { Colors } from '@blueprintjs/colors';
-import {
-  BufferGeometry,
-  Color,
-  DoubleSide,
-  Float32BufferAttribute,
-  InstancedMesh,
-  MathUtils,
-  Mesh,
-  MeshBasicMaterial,
-  Object3D,
-  PerspectiveCamera,
-  PlaneGeometry,
-  Scene,
-  ShaderChunk,
-  ShaderMaterial,
-  Timer,
-  Uniform,
-  WebGLRenderer,
-} from 'three';
-import { OrbitControls } from 'three/examples/jsm/Addons.js';
-import fragmentShader from './shader/test/fragment.glsl?raw';
-import vertexShader from './shader/test/vertex.glsl?raw';
 import './style.css';
 
 const size = {
@@ -29,126 +7,134 @@ const size = {
   pixelRatio: Math.min(2, devicePixelRatio),
 };
 
-const el = document.querySelector('#root');
+const el = document.querySelector('#root') as HTMLDivElement;
+el.style.display = 'flex';
+el.style.justifyContent = 'center';
+el.style.alignItems = 'center';
+el.style.height = '100vh';
+el.style.background = Colors.BLACK;
 
-const renderer = new WebGLRenderer({
-  alpha: true,
-  antialias: true,
-});
-renderer.setSize(size.width, size.height);
-renderer.setPixelRatio(size.pixelRatio);
-el?.append(renderer.domElement);
+const canvas = document.createElement('canvas');
+canvas.width = size.width * size.pixelRatio;
+canvas.height = size.height * size.pixelRatio;
+canvas.style.width = size.width + 'px';
+canvas.style.height = size.height + 'px';
+el?.append(canvas);
 
-const scene = new Scene();
-scene.background = new Color(Colors.BLACK);
+const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+ctx.scale(size.pixelRatio, size.pixelRatio);
 
-const camera = new PerspectiveCamera(70, size.width / size.height, 0.01, 1000);
-camera.position.set(0, 1, 1);
-camera.lookAt(scene.position);
-
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.enableDamping = true;
-
-const floorGeometry = new PlaneGeometry(2.15, 2.15, 32, 32);
-const floorMaterial = new MeshBasicMaterial({
-  color: 0x000000,
-});
-const floor = new Mesh(floorGeometry, floorMaterial);
-floor.rotation.x = -Math.PI / 2;
-scene.add(floor);
-
-const timer = new Timer();
-
-const positionsArr = new Float32Array([
-  //
-  1.0, 0.0, 0.0,
-  //
-  0.0, 1.0, 0.0,
-  //
-  -1.0, 0.0, 0.0,
-]);
-
-const uvArr = new Float32Array([
-  //
-  1.0, 0.0,
-  //
-  0.5, 1.0,
-  //
-  0.0, 0.0,
-]);
-
-const grassbladeGeometry = new BufferGeometry();
-grassbladeGeometry.setAttribute(
-  'position',
-  new Float32BufferAttribute(positionsArr, 3),
-);
-grassbladeGeometry.setAttribute('uv', new Float32BufferAttribute(uvArr, 2));
-
-const uniforms = {
-  uColor: new Uniform(new Color('#7CFC00')),
-  uTime: new Uniform(0),
+const origin = {
+  x: size.width / 2,
+  y: size.height / 2,
 };
 
-const instance = {
-  count: 5000,
-};
+function clean() {
+  ctx.save();
 
-const grassbladeMaterial = new ShaderMaterial({
-  uniforms,
-  vertexShader,
-  fragmentShader,
-  side: DoubleSide,
-});
-const grass = new InstancedMesh(
-  grassbladeGeometry,
-  grassbladeMaterial,
-  instance.count,
-);
+  ctx.fillStyle = Colors.BLACK;
+  ctx.fillRect(0, 0, size.width, size.height);
 
-const obj = new Object3D();
-
-console.log(ShaderChunk['begin_vertex'], ShaderChunk['project_vertex']);
-
-for (let i = 0; i < instance.count; i++) {
-  const x = MathUtils.randFloat(-1, 1);
-  const z = MathUtils.randFloat(-1, 1);
-
-  obj.position.set(x, obj.position.y, z);
-  obj.scale.set(
-    MathUtils.randFloat(0.05, 0.1),
-    MathUtils.randFloat(0.07, 0.1),
-    0.1,
-  );
-  obj.updateMatrix();
-  obj.updateMatrixWorld();
-  obj.updateWorldMatrix(true, true);
-
-  grass.setMatrixAt(i, obj.matrix);
+  ctx.restore;
 }
 
-scene.add(grass);
+function renderOrigin() {
+  {
+    ctx.save();
 
-function render() {
-  // UPDATE
-  timer.update();
-  controls.update();
+    ctx.beginPath();
+    ctx.fillStyle = Colors.CERULEAN3;
+    ctx.translate(origin.x, origin.y);
+    ctx.arc(0, 0, 10, 0, Math.PI * 2);
+    ctx.fill();
 
-  const dt = timer.getDelta();
-  uniforms.uTime.value += dt;
+    ctx.restore();
+  }
+  {
+    ctx.save();
 
-  // RENDER
-  renderer.render(scene, camera);
+    ctx.beginPath();
+    ctx.fillStyle = 'rgb(20, 126, 179, 0.3)';
+    ctx.strokeStyle = Colors.CERULEAN3;
+
+    ctx.translate(origin.x, origin.y);
+    ctx.arc(0, 0, 20, 0, Math.PI * 2);
+    ctx.fill();
+
+    ctx.stroke();
+
+    ctx.restore();
+  }
+}
+
+function renderRange() {
+  ctx.save();
+
+  ctx.beginPath();
+  ctx.fillStyle = 'rgb(20, 126, 179, 0.1)';
+  ctx.strokeStyle = Colors.CERULEAN3;
+
+  ctx.translate(origin.x, origin.y);
+  ctx.arc(0, 0, 80, 0, Math.PI * 2);
+  ctx.fill();
+
+  ctx.lineWidth = 1;
+  ctx.lineDashOffset = offset * -10;
+  ctx.setLineDash([5, 3]);
+  ctx.stroke();
+
+  ctx.restore();
+}
+
+function renderDirection() {
+  ctx.save();
+  ctx.translate(origin.x, origin.y);
+
+  ctx.beginPath();
+  ctx.moveTo(20, 0);
+  ctx.lineTo(36, 0);
+  ctx.lineWidth = 1;
+  ctx.strokeStyle = Colors.CERULEAN3;
+  ctx.stroke();
+  ctx.closePath();
+
+  ctx.restore();
+
+  ctx.save();
+
+  const p = new Path2D(
+    'M6.3771 12.4679L3.94323 18.0159C3.24249 19.6132 4.95015 21.1979 6.49094 20.3797L19.7145 13.3532C21.0163 12.661 21.017 10.7949 19.7145 10.1034L6.49165 3.07755C4.95157 2.25872 3.24249 3.84476 3.94394 5.44141L6.3778 10.9894C6.47995 11.2223 6.5327 11.4739 6.5327 11.7283C6.5327 11.9827 6.47995 12.2343 6.3778 12.4672',
+  );
+
+  ctx.translate(origin.x + 24, origin.y - 12);
+
+  ctx.fillStyle = Colors.CERULEAN3;
+  ctx.fill(p);
+
+  ctx.restore();
+}
+
+let prevTime = 0;
+let dt = 0;
+let offset = 0;
+
+function render(time: number = 0) {
+  dt = (time - prevTime) / 1000;
+  prevTime = time;
+
+  offset += dt;
+
+  clean();
+
+  renderOrigin();
+  renderRange();
+  renderDirection();
   // ANIMATION
   requestAnimationFrame(render);
 }
 render();
 
-window.addEventListener('resize', () => {
-  size.width = window.innerWidth;
-  size.height = window.innerHeight;
-
-  renderer.setSize(size.width, size.height);
-
-  camera.aspect = size.width / size.height;
-  camera.updateProjectionMatrix();
+canvas.addEventListener('pointermove', (e) => {
+  origin.x = e.offsetX;
+  origin.y = e.offsetY;
 });
