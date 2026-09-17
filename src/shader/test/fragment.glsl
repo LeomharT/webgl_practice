610024489912534a97867faa44926a5d2934adc8
LeomharT <leo.liao.lzy@gmail.com>
2026-09-17 21:52:54 +0800

    feat:center the uv

--- src/shader/test/fragment.glsl
+++ src/shader/test/fragment.glsl
@@ -3,8 +3,11 @@ varying vec2 vUv;
 void main() {
   vec3 color = vec3(1.0);
   vec2 uv = vUv;
+  uv -= 0.5;
 
-  color = vec3(uv.x);
+  float angle = atan(uv.x, uv.y);
+
+  color = vec3(angle);
 
   gl_FragColor = vec4(color, 1.0);
 }
