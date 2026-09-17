f5837c716928c576df9b5e5b3eaa20249d4f4292
LeomharT <leo.liao.lzy@gmail.com>
2026-09-17 21:08:44 +0800

    feat: do it both way

--- src/shader/test/fragment.glsl
+++ src/shader/test/fragment.glsl
@@ -1,5 +1,10 @@
+varying vec2 vUv;
+
 void main() {
   vec3 color = vec3(1.0);
+  vec2 uv = vUv;
+
+  color = vec3(uv.x);
 
   gl_FragColor = vec4(color, 1.0);
 }
--- src/shader/test/vertex.glsl
+++ src/shader/test/vertex.glsl
@@ -1,4 +1,9 @@
+varying vec2 vUv;
+
 void main() {
   #include <begin_vertex>
   #include <project_vertex>
+
+  // VARYING
+  vUv = uv;
 }
