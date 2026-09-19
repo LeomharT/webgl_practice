eddaab89b6577960da7c5d74eb668c5455d085eb
LeomharT <leo.liao.lzy@gmail.com>
2026-09-19 11:43:20 +0800

    Adjust radar scan green intensity to 0.7

--- src/shader/test/fragment.glsl
+++ src/shader/test/fragment.glsl
@@ -26,7 +26,7 @@ void main() {
 
   uv += 0.5;
 
-  color = mix(vec3(0.0), vec3(0.0, 1.0, 0.0), angle);
+  color = mix(vec3(0.0), vec3(0.0, 0.7, 0.0), angle);
 
   float dist = distance(uv, vec2(0.5));
   dist = smoothstep(0.2, 0.5, dist);
