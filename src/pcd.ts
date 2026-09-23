2ecc66b0a2bd6f0222a1a1ab7f75ff82c098d0d9
LeomharT <leo.liao.lzy@gmail.com>
2026-09-23 13:57:22 +0800

    feat: learn webgpu is alright

--- src/pcd.ts
+++ src/pcd.ts
@@ -1,40 +1,27 @@
 import { Colors } from '@blueprintjs/colors';
-import * as EssentialsPlugin from '@tweakpane/plugin-essentials';
+import CameraControls from 'camera-controls';
 import {
-  AxesHelper,
-  Box3Helper,
-  BufferGeometry,
   Color,
   PerspectiveCamera,
-  Points,
   Scene,
-  ShaderMaterial,
-  Uniform,
-  Vector2,
-  WebGLRenderer,
-} from 'three';
-import { OrbitControls, PCDLoader } from 'three/examples/jsm/Addons.js';
-import { Pane } from 'tweakpane';
-import fragmentShader from './shader/pcd/fragment.glsl?raw';
-import vertexShader from './shader/pcd/vertex.glsl?raw';
+  Timer,
+  WebGPURenderer,
+} from 'three/webgpu';
 import './style.css';
 
 const sizes = {
   width: window.innerWidth,
   height: window.innerHeight,
-  pixelRatio: Math.min(2, window.devicePixelRatio),
+  pixelRatio: Math.max(2, window.devicePixelRatio),
 };
 
 const el = document.querySelector('#root');
 
-// LOADER
-const pcdLoader = new PCDLoader();
-
-// BASIC
-const renderer = new WebGLRenderer({
-  antialias: true,
+const renderer = new WebGPURenderer({
   alpha: true,
+  antialias: true,
 });
+await renderer.init();
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(sizes.pixelRatio);
 el?.append(renderer.domElement);
@@ -45,89 +32,27 @@ scene.background = new Color(Colors.BLACK);
 const camera = new PerspectiveCamera(
   70,
   sizes.width / sizes.height,
-  0.01,
-  1000,
+  0.1,
+  10000,
 );
-camera.position.set(3, 5, 3);
+camera.position.set(0, 3, 3);
 camera.lookAt(scene.position);
 
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.enableDamping = true;
-controls.dampingFactor = 0.08;
+console.log(renderer.domElement);
 
-// WORLD
-const uniforms = {
-  uSize: new Uniform(0.02),
-  uResolution: new Uniform(new Vector2(sizes.width, sizes.height)),
-  uMinHeight: new Uniform(0),
-  uMaxHeight: new Uniform(0),
-};
+const controls = new CameraControls(camera, renderer.domElement);
+controls.enabled = true;
 
-pcdLoader.load('/park081302.pcd', (data) => {
-  const geometry = new BufferGeometry();
-  geometry.setAttribute('position', data.geometry.attributes.position);
-  geometry.rotateX(-Math.PI / 2);
+const timer = new Timer();
 
-  const material = new ShaderMaterial({
-    uniforms,
-    vertexShader,
-    fragmentShader,
-  });
-
-  geometry.computeBoundingBox();
-  geometry.computeBoundingSphere();
-
-  if (geometry.boundingBox) {
-    uniforms.uMaxHeight.value = geometry.boundingBox.max.y;
-    uniforms.uMinHeight.value = -geometry.boundingBox.max.y;
-  }
-  if (geometry.boundingSphere) controls.target = geometry.boundingSphere.center;
-
-  const points = new Points(geometry, material);
-
-  //   points.rotation.x = -Math.PI / 2;
-  scene.add(points);
-
-  const boxHelper = new Box3Helper(
-    geometry.boundingBox!,
-    new Color(Colors.TURQUOISE3),
-  );
-  scene.add(boxHelper);
-});
-
-const axesHelper = new AxesHelper(30);
-scene.add(axesHelper);
-
-const pane = new Pane({ title: 'Debug Pane' });
-pane.element.parentElement!.style.width = '380px';
-// Register plugin to the pane
-pane.registerPlugin(EssentialsPlugin);
-
-// Add a FPS graph
-const fpsGraph: any = pane.addBlade({
-  view: 'fpsgraph',
-  label: undefined,
-  rows: 3,
-});
-
-pane.addBinding(uniforms.uSize, 'value', {
-  label: 'Size',
-  min: 0,
-  max: 0.1,
-});
-
-// EVENTS
 function render() {
-  fpsGraph.begin();
-
-  // update
-  controls.update();
-  // render
+  // UPDATE
+  timer.update();
+  controls.update(timer.getDelta());
+  // RENDER
   renderer.render(scene, camera);
-  // animation
+  // ANIMTATION
   requestAnimationFrame(render);
-
-  fpsGraph.end();
 }
 render();
 
